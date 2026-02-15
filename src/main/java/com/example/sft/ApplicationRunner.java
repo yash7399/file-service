d7c8fe652db8ae31009620f6057491964d9e89d6
Yash Shukla <119918899+yash7399@users.noreply.github.com>
2026-02-15 16:24:17 +0530

    Update ApplicationRunner.java

--- src/main/java/com/example/sft/ApplicationRunner.java
+++ src/main/java/com/example/sft/ApplicationRunner.java
@@ -1,5 +1,6 @@
-package com.example.sft;
+package com.ncdex.filetransfer;
 
+import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,11 +19,18 @@ import org.apache.logging.log4j.core.LoggerContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import com.example.sft.config.FileConfig;
-import com.example.sft.constants.GlobalConstants;
+
 import com.hierynomus.smbj.session.Session;
 import com.hierynomus.smbj.share.DiskShare;
 import com.jcraft.jsch.ChannelSftp;
+import com.ncdex.filetransfer.config.FileConfig;
+import com.ncdex.filetransfer.connections.NASConnection;
+import com.ncdex.filetransfer.connections.SFTPConnection;
+import com.ncdex.filetransfer.constants.GlobalConstants;
+import com.ncdex.filetransfer.emails.Emails;
+import com.ncdex.filetransfer.utils.Prop;
+import com.ncdex.filetransfer.utils.Trigger;
+import com.ncdex.filetransfer.utils.Utils;
 
 @Component
 public class ApplicationRunner implements CommandLineRunner {
@@ -30,10 +38,10 @@ public class ApplicationRunner implements CommandLineRunner {
 	private static final Logger log = LogManager.getLogger(ApplicationRunner.class);
 
 	private static String batchDate;
-	private static String configPath;
+//	private static String configPath;
 
 	@Autowired
-	Util util;
+	Utils util;
 
 	@Autowired
 	Emails emails;
@@ -41,37 +49,79 @@ public class ApplicationRunner implements CommandLineRunner {
 	@Autowired
 	FileTransferService File_Transfer_Test;
 
-	private final ConcurrentMap<String, List<String>> missingFiles = new ConcurrentHashMap<>();
+	private final static ConcurrentMap<String, List<String>> missingFiles = new ConcurrentHashMap<>();
 
-	private final ConcurrentMap<String, List<String>> transferedFiles = new ConcurrentHashMap<>();
+	private final static ConcurrentMap<String, List<String>> transferedFiles = new ConcurrentHashMap<>();
 
-	private List<String> departments = new ArrayList();
+	private static List<String> departments = new ArrayList();
 
 	public static String getBatchDate() {
 		return batchDate;
 	}
-	
-	public static String getConfigPath() {
-		return configPath;
+
+	public static ConcurrentMap<String, List<String>> getMissingFiles() {
+		return missingFiles;
 	}
-	
-	
+
+	public static ConcurrentMap<String, List<String>> getTransferedFiles() {
+		return transferedFiles;
+	}
+
+	public static List<String> getDepartments() {
+		return departments;
+	}
+
 //	@Override
-//	public void run(String...args) {
+//	public void run(String... args) throws Exception {
+//		if (args.length < 2) {
+//			System.exit(1);
+//		}
+//
 //		this.batchDate = args[0];
-//		this.configPath=args[2];
-//		System.out.println(configPath);
-//		Prop.init(configPath);
-//	
-////		System.out.println(prop.getProperty("01.ip"));
+//
+//		String connectionCode = args[1];
+//
+//		String configpath = null;
+//
+//		for (String arg : args) {
+//			if (arg.startsWith("--spring.config.location=")) {
+//
+//				configpath = arg.split("=", 2)[1];
+//				System.out.println(configpath);
+//				break;
+//			}
+//
+//		}
+//
+//		if (configpath == null) {
+//			System.out.print("NO config path");
+//			System.exit(1);
+//		}
+//
+//		Prop.init(configpath);
+//		Properties prop = Prop.getProp();
+//
+//		Path path = Paths.get(prop.getProperty("path.log4j2"));
+//
+//		if (!Files.exists(path)) {
+//			throw new RuntimeErrorException(null, "Log4j2.xml file does not exists " + path);
+//		}
+//		LoggerContext context = (LoggerContext) LogManager.getContext(false);
+//
+//		URI configUri = Path.of(prop.getProperty("path.log4j2")).toUri();
+//
+//		context.setConfigLocation(configUri);
+//
+//		departments.add("Enforcement");
+//
 //		emails.connectionIssue();
+//
 //	}
 
 	@Override
 	public void run(String... args) {
 		
-		if(args.length<3) {
-			log.info("Please enter correct arguments ");
+		if(args.length<2) {
 			System.exit(1);
 		}
 		
@@ -79,8 +129,25 @@ public class ApplicationRunner implements CommandLineRunner {
 		
 		String connectionCode=args[1];
 		
-		this.configPath=args[2];
-		Prop.init(configPath);
+		
+		String configpath=null;
+		
+		for(String arg:args) {
+			if(arg.startsWith("--spring.config.location=")) {
+				configpath=arg.split("=",2)[1];
+				System.out.println(configpath);
+				break;
+			}
+					
+		}
+		
+		
+		if(configpath==null) {
+			System.out.print("NO config path");
+			System.exit(1);
+		}
+		
+		Prop.init(configpath);
 		Properties prop=Prop.getProp();
 		
 		Path path = Paths.get(prop.getProperty("path.log4j2"));
@@ -103,7 +170,7 @@ public class ApplicationRunner implements CommandLineRunner {
 		int noOfThreads;
 		if(GlobalConstants.no_of_threads==0) {
 			noOfThreads=1;
-			log.info("Give proper no of threads");
+			log.info("Give no of threads by default taking 1");
 		}
 		else {
 			noOfThreads=GlobalConstants.no_of_threads;
@@ -115,19 +182,24 @@ public class ApplicationRunner implements CommandLineRunner {
 			Map<String,Map<String, List<FileConfig>>> departmentFiles = util.getMap();
 			
 			for (Map.Entry<String, Map<String, List<FileConfig>>> entry : departmentFiles.entrySet()) {
+				departments.add(entry.getKey());
+			}
+			
+			for (Map.Entry<String, Map<String, List<FileConfig>>> entry : departmentFiles.entrySet()) {
 				
 				String department = entry.getKey();
 				Map<String, List<FileConfig>> records = entry.getValue();
-				departments.add(department);
 				
 				threadPool.submit(() -> processDepartment(connectionCode , department, records, args));
+				
 			}
-
+			
 			threadPool.shutdown();
 
 			while (!threadPool.awaitTermination(5, TimeUnit.MINUTES)) {
 				log.info("Waiting for thread for ending task");
 			}
+			
 			log.info("*********** APPLICATION FINISHED ***********");
 
 			System.exit(1);
@@ -146,11 +218,14 @@ public class ApplicationRunner implements CommandLineRunner {
 		
 		Properties prop=Prop.getProp();
 		
-		DiskShare share = null;
 		
 		for(Map.Entry<String, List<FileConfig>> entry:connections.entrySet()) {
 			
 			ChannelSftp sftp=null;
+			
+			Session session=null;
+			DiskShare share = null;
+			
 			if(connectionCode.equals("00")  || connectionCode.equals(entry.getKey())) {
 
 				 sftp = SFTPConnection.connect(entry.getKey());
@@ -158,7 +233,7 @@ public class ApplicationRunner implements CommandLineRunner {
 			else {
 				return;
 			}
-			Session session = NASConnection.connect();
+			session = NASConnection.connect();
 			
 			log.info("Department execution started:" + department);
 			System.out.println("Department execution started:" + department);
@@ -182,7 +257,7 @@ public class ApplicationRunner implements CommandLineRunner {
 				System.out.println("Transfering file "+fileName);
 				log.info("Transfering file "+fileName);
 				try {
-					File_Transfer_Test.transfer(sftp,entry.getKey(), share, fileName, record.getShare(), record.getSource(),
+					File_Transfer_Test.transfer(entry.getKey(),sftp,entry.getKey(), share, fileName, record.getShare(), record.getSource(),
 							record.getDestination(), record.getTime(), department, missingFiles, transferedFiles);
 					
 				} catch (Exception e) {
@@ -190,28 +265,34 @@ public class ApplicationRunner implements CommandLineRunner {
 					log.error("File transfer failed for {} in department {}", fileName, department, e);
 				}
 			}
-		}
-
-		
-		emails.filesNotFound(missingFiles, transferedFiles, department);
-		
-
-		String triggerFolder = prop.getProperty("trigger.folder." + department);
-
-		try {
-			if(triggerFolder!=null) {
-				
-				Trigger.makeTriggerFile(share, triggerFolder);
-				System.out.println("Sucessfully created trigger file for " + department);
-				log.info("Sucessfully created trigger file for " + department);
+			String triggerFolder = prop.getProperty("trigger.folder." + department);
+			
+			try {
+				if(triggerFolder!=null) {
+					Trigger.makeTriggerFile(share, triggerFolder,entry.getKey());
+					System.out.println("Sucessfully created trigger file for " + department);
+					log.info("Sucessfully created trigger file for " + department);
+				}
+				else {
+					log.info("Trigger folder is null. Cannot create trigger file ");
+				}
+			} catch (Exception e) {
+				System.out.println("Cannot create trigger file for department " + department);
+				log.info("Cannot create trigger file for department " + department);
+				e.printStackTrace();
 			}
-			else {
-				log.info("Trigger folder is null. Cannot create trigger file ");
+			finally {
+				try {
+					share.close();
+					session.close();
+					sftp.disconnect();
+					log.info("Every session is closed");
+				} catch (IOException e) {
+					log.error("Error while closing sessions");
+				}
 			}
-		} catch (Exception e) {
-			System.out.println("Cannot create trigger file for department " + department);
-			log.info("Cannot create trigger file for department " + department);
-			e.printStackTrace();
 		}
+		emails.filesNotFound(missingFiles, transferedFiles, department,false);
+
 	}
 }
